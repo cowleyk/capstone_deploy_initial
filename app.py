1bcf6748c7824594d499130bfda8f329bb4f2d64
Kevin Cowley <cowley.kevin.l@gmail.com>
2017-03-06 12:33:01 -0700

    python manage.py db init successful

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -9,7 +10,10 @@ load_dotenv(dotenv_path)
 app = Flask(__name__)
 
 app.config.from_object(os.environ.get('APP_SETTINGS'))
-print(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Result
 
 @app.route('/')
 def hello():
