704196f78effebfaaec5a0637bb3c085666dc0ee
Kevin Cowley <cowley.kevin.l@gmail.com>
2017-03-06 12:21:36 -0700

    config.py added and APP_SETTINGS incorp_d

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+
 app = Flask(__name__)
 
+app.config.from_object(os.environ.get('APP_SETTINGS'))
+print(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
