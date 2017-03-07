60328d134bf7024fb94673e46b3986d4925ab515
Kevin Cowley <cowley.kevin.l@gmail.com>
2017-03-06 17:36:05 -0700

    fixing ng errors

--- static/app.config.js
+++ static/app.config.js
@@ -16,27 +16,27 @@
         url: '/',
         component: 'home',
       })
-      .state({
-        name: 'csvmanage',
-        url: '/csvmanage',
-        component: 'csvmanage',
-      })
-      .state({
-        name: 'regression',
-        url: '/regression',
-        component: 'regression',
-        params: {
-          matrixObj: {
-            C: 'no data',
-            bHat: 'no data',
-            X: 'no data',
-            y: 'no data',
-            n: 'no data',
-            k: 'no data',
-            p: 'no data'
-          }
-        },
-      });
+//      .state({
+//        name: 'csvmanage',
+//        url: '/csvmanage',
+//        component: 'csvmanage',
+//      })
+//      .state({
+//        name: 'regression',
+//        url: '/regression',
+//        component: 'regression',
+//        params: {
+//          matrixObj: {
+//            C: 'no data',
+//            bHat: 'no data',
+//            X: 'no data',
+//            y: 'no data',
+//            n: 'no data',
+//            k: 'no data',
+//            p: 'no data'
+//          }
+//        },
+//      });
   }
 
 }());
